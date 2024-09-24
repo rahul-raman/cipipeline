d3355833a1aa5013d5f5e4ca99c243e330f228b7
Rahul Raman <rahulramansingh@hotmail.com>
2024-09-24 13:50:22 +0530

    testing setup

--- src/test/java/test1/Test01.java
+++ src/test/java/test1/Test01.java
@@ -1,25 +1,29 @@
 package test1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
 
 public class Test01 {
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup(); // Call setup here
+        driver = new ChromeDriver(); // Initialize driver here
+    }
 
     @Test
-    public void hi()
-    {
+    public void hi() {
         System.out.println("hello");
         driver.get("https://in.bookmyshow.com/");
-
-
     }
 
     @AfterTest
-    public void wrapup()
-    {
+    public void wrapup() {
         driver.quit();
     }
 }
